9ec8dff57c5491dec80e3066c9f5cc476fd277e6
goldsmit409 <goldsmit409@outlook.com>
2014-11-27 02:20:48 +0530

    Update 2
    Hash code

--- App2/CallApi.cs
+++ App2/CallApi.cs
@@ -2,6 +2,9 @@ using System;
 using RestSharp;
 using RestSharp.Deserializers;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace App2
 {
@@ -11,7 +14,6 @@ namespace App2
 
         public static string Fetch(string Name, String Location)
         {
-
             string api = "api/person/" + Name + "/hello/" + Location;
             RestRequest request = new RestRequest(api);
             try
@@ -23,40 +25,56 @@ namespace App2
                     return content;
                 }
                 else
-                    return resp.ErrorException.ToString()+resp.ErrorMessage;
+                    return resp.ErrorException.ToString() + resp.ErrorMessage;
             }
             catch (Exception ex)
             {
                 return ex.Message;
             }
-
         }
 
         public static IRestResponse getMovies()
         {
             string api = "api/movie/list";
             RestRequest request = new RestRequest(api);
+            request.AddHeader("px-hash", request.AddHash(""));
             request.RequestFormat = DataFormat.Json;
-            //try
-            //{
             IRestResponse resp = client.Execute(request);
-            // if (resp.StatusCode == System.Net.HttpStatusCode.OK)
-            //{
             var content = resp.Content;
-            //JsonDeserializer deserial = new JsonDeserializer();
-            //List<Movie> movieList = deserial.Deserialize<List<Movie>>(resp);
-            //var JSONObj = deserial.Deserialize<Dictionary<string, string>>(resp);
-            //int rowCount = JSONObj["Count"];
-            // DeserializeRxTerm(resp.Content);
             return resp;
-            //}
-            //else
-            // return resp.ErrorMessage;
-            //}
-            //catch (Exception ex)
-            //{
-            //    return "Try Again";
-            //}
+        }
+    }
+    public static class ExtensionMethods
+    {
+        public static string AddHash(this RestRequest RequestObject, string SharedKey)
+        {
+            var parameters = RequestObject.Parameters.OrderBy(p => p.Name).ToList();
+            string[] paramType = { "QueryString", "RequestBody", "HttpHeader", "Cookie" };
+            foreach (string type in paramType)
+            {
+                SharedKey = appendHashString(SharedKey, type, parameters);
+            }
+            UnicodeEncoding ue = new UnicodeEncoding();
+            byte[] HashValue;
+            byte[] sharedkey = ue.GetBytes(SharedKey);
+            SHA256Managed sha256 = new SHA256Managed();
+            string hashString = string.Empty;
+            HashValue = sha256.ComputeHash(sharedkey);
+            foreach (byte b in HashValue)
+            {
+                hashString += string.Format("{0:X2}", b);
+            }
+            return hashString;
+        }
+
+        private static string appendHashString(string SharedKey, string type, List<Parameter> parameters)
+        {
+            var param = from p in parameters where p.Type.ToString() == type select p;
+            foreach (var item in param)
+            {
+                SharedKey += item.Name + item.Value;
+            }
+            return SharedKey;
         }
     }
 }
